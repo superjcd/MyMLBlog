9d2ab7db01849e823b346246ee3e3901ada6bda2
蒋超迪 <jiangchaodi@jiangchaodideMacBook-Pro.local>
2020-02-04 18:58:13 +0800

    first view

--- mlblogsystem/blog/views.py
+++ mlblogsystem/blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+ 
+def index(request):
+    return render(request, "blog/index.html", context={
+        "title":'我的博客',
+        "welcome":'欢迎阅读',
+    })
+    #return HttpResponse("欢迎访问我的博客首页！")
